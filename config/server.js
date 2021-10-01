3311148f7bb1e4514f77b250f79e95003065c968
shereenashajahan01 <80378536+shereenashajahan01@users.noreply.github.com>
2021-10-01 08:10:28 +0400

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bc5504ce6434a69388e03e4548e6549c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'be3ff89e-6460-4b26-ab39-debf78140afe'
+};
