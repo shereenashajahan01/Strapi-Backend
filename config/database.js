75e092ae42c653417ca04e29fff0cb3844cd3fd7
shereenashajahan01 <80378536+shereenashajahan01@users.noreply.github.com>
2021-10-01 08:34:37 +0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,5 @@
-const { parse } = require("pg-connection-string");
 
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("postgres://vkeonldoimljev:527ca61420721afe51c4397c42fcb249628d8fcb692d6b82bfb7e18f9f344dfd@ec2-44-199-26-122.compute-1.amazonaws.com:5432/daqom94cuir0e9"));
-  return{
+module.exports = ({ env }) => ({
   defaultConnection: "default",
   connections: {
     default: {
@@ -15,12 +12,11 @@ module.exports = ({ env }) => {
         username: env("DATABASE_USERNAME", "vkeonldoimljev"),
         password: env("DATABASE_PASSWORD", "527ca61420721afe51c4397c42fcb249628d8fcb692d6b82bfb7e18f9f344dfd"),
         schema: env("DATABASE_SCHEMA", "public"),
-        ssl: { rejectUnauthorized: false }
+      ssl: { rejectUnauthorized: false }
       },
        options: {
           ssl: false
         },
     },
   },
-}
-};
+});
