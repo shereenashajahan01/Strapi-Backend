cae84f08ce52aa2bf9fb7d4a94af61990afeaefa
shereenashajahan01 <80378536+shereenashajahan01@users.noreply.github.com>
2021-10-01 08:26:39 +0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -9,11 +9,11 @@ module.exports = ({ env }) => {
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
+        host: env("DATABASE_HOST", "ec2-44-199-26-122.compute-1.amazonaws.com"),
         port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "uaefoodplatform"),
-        username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "postgres"),
+        database: env("DATABASE_NAME", "daqom94cuir0e9"),
+        username: env("DATABASE_USERNAME", "vkeonldoimljev"),
+        password: env("DATABASE_PASSWORD", "527ca61420721afe51c4397c42fcb249628d8fcb692d6b82bfb7e18f9f344dfd"),
         schema: env("DATABASE_SCHEMA", "public"),
         ssl: { rejectUnauthorized: false }
       },
